4e711ac0ad320c648c7270aca440afdb13696222
kienan <leung.kienan@gmail.com>
2023-03-05 21:43:39 +1300

    tinkering with home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,20 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { AspectRatio, Container, Text } from '@chakra-ui/react'
 import { Center, Square, Circle } from '@chakra-ui/react'
 import { Flex, Spacer } from '@chakra-ui/react'
 import { Link } from '@chakra-ui/react'
-import { ExternalLinkIcon } from '@chakra-ui/icons'
+import { Divider } from '@chakra-ui/react'
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Button,
+  Heading,
+  Image,
+} from '@chakra-ui/react'
+// import { ExternalLinkIcon } from '@chakra-ui/icons'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,14 +29,34 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <header>
-        <Link id="nav-home">Hi! Welcome</Link>
+        <Link id="nav-home">Kienan Leung</Link>
         <div>
           <Link className="navBar">About</Link>
           <Link className="navBar">Projects</Link>
           <Link className="navBar">Contact</Link>
         </div>
       </header>
-      <div className="main">
+
+      <Card width="60%" margin="10px auto" align="center">
+        <CardHeader>
+          <Heading size="2xl">This is me</Heading>
+        </CardHeader>
+        <CardBody textAlign="center">
+          <Image
+            src="/kienan.jpg"
+            className="myself"
+            alt="kienan"
+            width="400px"
+            boxSize={'200px'}
+            borderRadius="full"
+            fit="cover"
+          />
+          <br></br>
+          <Text>Hi there!</Text>
+        </CardBody>
+      </Card>
+
+      {/* <div className="main">
         <div className="main__content">
           <AspectRatio maxW="400px" ratio={4 / 3}>
             <Image
@@ -41,11 +70,20 @@ export default function Home() {
           <div>
             <h1 className="desc">My name is Kienan</h1>
             <Center>
-              <Text>I am a</Text>
+              <Text>I am a:</Text>
+            </Center>
+            <Center height="50px">
+              <Text>Developer</Text>
+              <Divider
+                orientation="vertical"
+                borderColor={'red'}
+                margin={'5px'}
+              />
+              <Text>Accountant</Text>
             </Center>
           </div>
         </div>
-      </div>
+      </div> */}
     </>
   )
 }
